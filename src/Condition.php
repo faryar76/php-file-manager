13440aa584d9948801503e38d43fdf5f5dfbe981
faryar <faryar76@hotmil.com>
2019-02-09 21:23:00 +0330

    refactor

--- src/Condition.php
+++ src/Condition.php
@@ -54,14 +54,18 @@ class Condition
     {
         switch (true) {
             case (!is_array($args)):
-                throw new \InvalidArgumentException(sprintf("input must be array %s given",gettype($args)));
+            throw new \InvalidArgumentException(sprintf("input must be array %s given",gettype($args)));
+
             case (!in_array($args[0],$this->fields)):
-                throw new \InvalidArgumentException(sprintf("fields [%s] not found!",$args[0]));
+            throw new \InvalidArgumentException(sprintf("fields [%s] not found!",$args[0]));
+
             case (count($args) == 3 && !in_array(strtolower($args[1]),$this->oparators)):
-                throw new \InvalidArgumentException(sprintf("oparator [%s] not found!",$args[1]));
+            throw new \InvalidArgumentException(sprintf("oparator [%s] not found!",$args[1]));
+
             case (count($args) > 3 || count($args) < 2):
             throw new \InvalidArgumentException(sprintf("input  array count must be 2 %s given",count($args)));
-        default:
+
+            default:
             return true;    
         }
     }
